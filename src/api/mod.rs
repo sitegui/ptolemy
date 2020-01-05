abef3ecbb884bd239b2fe94d3cc1d752fe370a86
Guilherme <sitegui@sitegui.com.br>
2020-01-05 18:00:44 +0100

    Finish first version of /route

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -3,6 +3,8 @@ mod data_types;
 use crate::cartograph::*;
 use actix_web::{get, web, App, HttpServer, Responder};
 use data_types::*;
+use geo_types::line_string::LineString;
+use polyline::encode_coordinates;
 use std::path::Path;
 
 #[get("/route/v1/driving/{coordinates}")]
@@ -13,6 +15,21 @@ async fn route(coords: web::Path<Coordinates>, carto: web::Data<Cartograph>) -> 
         .iter()
         .map(|point| carto.project(point).unwrap())
         .collect();
+
+    // Calculate each path and accumulate all them
+    let mut route_points: Vec<[f64; 2]> = Vec::new();
+    let mut distance = 0.;
+    for points in waypoints.windows(2) {
+        let graph_path = carto.shortest_path(&points[0], &points[1]).unwrap();
+        distance += graph_path.distance as f64;
+        route_points.extend(
+            graph_path
+                .points
+                .into_iter()
+                .map(|point| [point.lon.as_degrees(), point.lat.as_degrees()]),
+        );
+    }
+    let path_line = LineString::from(route_points);
 
     web::Json(RouteResponse {
         waypoints: waypoints
@@ -25,7 +42,10 @@ async fn route(coords: web::Path<Coordinates>, carto: web::Data<Cartograph>) -> 
                 ],
             })
             .collect(),
-        routes: vec![],
+        routes: vec![RouteItemResponse {
+            distance,
+            geometry: encode_coordinates(path_line, 5).unwrap(),
+        }],
     })
 }
 
